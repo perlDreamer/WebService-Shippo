a3fcac69a92a7097d1a0f94a4c6a7fc73716578d
Iain Campbell <cpanic@cpan.org>
2015-10-15 22:20:02 +0100

    Shippo::Updater update method works!

--- lib/WebService/Shippo/Updater.pm
+++ lib/WebService/Shippo/Updater.pm
@@ -7,7 +7,10 @@ require WebService::Shippo::Request;
 
 sub update
 {
-    ;
+    my ( $self, $object_id, $params ) = @_;
+    my $url = $self->url($object_id);
+    my $response = WebService::Shippo::Request->put( $url, $params );
+    return $self->construct_from( $response );
 }
 
 1;
