7d99d1e1b1e7273c0a19869168082ab56e7b5d81
Iain Campbell <cpanic@cpan.org>
2015-10-16 11:49:46 +0100

    Re-factored WebService::Shippo::Config -
    * parse_config_file is now load_config_file because we
      reload_config_file not reparse_config_file.
      
    * the module now searches a list of candidate config files for one that
      exists, if config_file is undefined.
      
    * code in WebService::Shippo that relied on the config file being loaded
      in order to then poke values into various places has now been moved
      into load_config_file to improve SoC.

--- lib/WebService/Shippo/Config.pm
+++ lib/WebService/Shippo/Config.pm
@@ -2,16 +2,40 @@ use strict;
 use warnings;
 
 package WebService::Shippo::Config;
-use Carp ( 'croak' );
-use YAML::XS ();
+require WebService::Shippo::Resource;
+require WebService::Shippo::Request;
+use Cwd;
+use Carp          ( 'croak' );
+use File::HomeDir ();
+use Path::Class   ();
+use YAML::XS      ();
 use namespace::clean;
 
+#<<< PerlTidy, leave this:
+( my $temp = __FILE__ ) =~ s{\.\w+$}{.yml};
+
+our @SEARCH_PATH = (
+    Path::Class::Dir->new( getcwd )->file( '.shipporc' )->stringify,
+    Path::Class::Dir->new( File::HomeDir->my_home )->file( '.shipporc' )->stringify,
+    Path::Class::Dir->new( '', 'etc' )->file( 'shipporc' )->stringify,
+    $temp
+);
+#>>>
+
 {
-    ( my $value = __FILE__ ) =~ s{\.\w+$}{.yml};
+    my $value = undef;
 
     sub config_file
     {
         my ( $invocant, $new_value ) = @_;
+        unless ( $value ) {
+            for my $candidate ( @SEARCH_PATH ) {
+                if ( -e $candidate ) {
+                    $value = $candidate;
+                    last;
+                }
+            }
+        }
         return $value unless @_ > 1;
         $value = $new_value;
         return $invocant;
@@ -19,12 +43,12 @@ use namespace::clean;
 }
 
 {
-    my $value = __PACKAGE__->parse_config_file;
+    my $value = __PACKAGE__->load_config_file;
 
     sub reload_config
     {
         my ( $invocant ) = @_;
-        $value = $invocant->parse_config_file;
+        $value = $invocant->load_config_file;
         return $invocant;
     }
 
@@ -37,14 +61,14 @@ use namespace::clean;
     }
 }
 
-sub parse_config_file
+sub load_config_file
 {
     my ( $class ) = @_;
     $class = ref( $class ) || $class;
     my $config_file = $class->config_file;
-    # Return empty config if no Config.yml exists
+    # Return empty config if no config file exists
     return bless( {}, $class )
-        unless -e $config_file;
+        unless $config_file && -e $config_file;
     # Fetch the config content. By default, this should be defined in a
     # file called Config.yml, located in the same folder as the Config.pm
     # module.
@@ -56,8 +80,15 @@ sub parse_config_file
     return bless( {}, $class )
         unless $config_yaml;
     # Parse the YAML content; use an empty config if that yields nothing.
-    my $self = YAML::XS::Load( $config_yaml ) || {};
-    return bless( $self, $class );
+    my $config = YAML::XS::Load( $config_yaml ) || {};
+    my $default_token = $config->{default_token} || 'private_token';
+    my $api_key       = $config->{$default_token};
+    Shippo::Resource->api_private_token( $config->{private_token} );
+    Shippo::Resource->api_public_token( $config->{public_token} );
+    Shippo::Resource->api_key( $api_key );
+    Shippo::Request->headers->{Authorization} = "ShippoToken $api_key"
+        if $api_key;
+    return bless( $config, $class );
 }
 
 BEGIN {
