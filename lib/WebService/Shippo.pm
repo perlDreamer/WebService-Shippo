972c163292ac934dba134861ec9e6171ba24e48e
Iain Campbell <cpanic@cpan.org>
2015-10-16 11:52:06 +0100

    Re-factored WebService::Shippo -
    * Calls related to configuration now moved to WebService::Shippo::Config
      to improve separation of concerns.

--- lib/WebService/Shippo.pm
+++ lib/WebService/Shippo.pm
@@ -217,11 +217,7 @@ BEGIN {
     no warnings 'once';
     # There are some useful mutators defined elsewhere that I'd like to
     # make available (alias) via the root namespace.
-    *config            = *WebService::Shippo::Config::config;
-    *api_private_token = *WebService::Shippo::Resource::api_private_token;
-    *api_public_token  = *WebService::Shippo::Resource::api_public_token;
-    *api_key           = *WebService::Shippo::Resource::api_key;
-    *headers           = *WebService::Shippo::Request::headers;
+    *api_key = *WebService::Shippo::Resource::api_key;
     # Forcing the dev to always use CPAN's perferred "WebService::Shippo"
     # namespace is just cruel; allow the use of "Shippo", too.
     *Shippo:: = *WebService::Shippo::;
@@ -230,19 +226,9 @@ BEGIN {
 sub import
 {
     my ( $class ) = @_;
-    # Both public and private auth tokens are stored in Config.yml, which
-    # should be located in the same folder as Config.pm. Load that config
-    # file to get at the authentication data. You can't have mine, go and
-    # create your own.
-    my $config        = Shippo->config;
-    my $default_token = $config->{default_token} || 'private_token';
-    my $api_key       = $config->{$default_token};
-    Shippo->api_private_token( $config->{private_token} );
-    Shippo->api_public_token( $config->{public_token} );
-    Shippo->api_key( $api_key );
-    # Yeah, and set up that standard authorization header while we're at it.
-    Shippo->headers->{Authorization} = "ShippoToken $api_key"
-        if Shippo->api_key;
+    # Configure Shippo client on import
+    WebService::Shippo::Config->config;
+    # Pass call frame to Exporter's import for it's processing
     goto &Exporter::import;
 }
 
