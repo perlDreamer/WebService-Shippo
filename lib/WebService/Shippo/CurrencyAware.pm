44b357292e913575e4cc19a363215f786d7d6364
Iain Campbell <cpanic@cpan.org>
2015-10-16 13:32:50 +0100

    WebService::Shippo::CurrencyAware added
    * For Rate, Shipment and ReturnShipment entities to validate 
      currency codes.

---
+++ lib/WebService/Shippo/CurrencyAware.pm
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use MRO::Compat 'c3';
+
+package WebService::Shippo::CurrencyAware;
+use Locale::Codes::Currency;
+use namespace::clean;
+
+{
+    my @codes = all_currency_codes;
+    my %codes = map { $_ => code2currency( $_ ) || '' } @codes;
+
+    sub validate_currency
+    {
+        my ( $invocant, $currency_code ) = @_;
+        my $k = lc( $currency_code );
+        return
+            unless exists( $codes{$k} );
+        return ( uc( $currency_code ), $codes{$k} )
+            if wantarray;
+        return uc( $currency_code );
+    }
+}
+
+1;
--- lib/WebService/Shippo/Shipment.pm
+++ lib/WebService/Shippo/Shipment.pm
@@ -3,17 +3,33 @@ use warnings;
 use MRO::Compat 'c3';
 
 package WebService::Shippo::Shipment;
+use Carp ( 'croak' );
+use Locale::Codes::Currency;
+use namespace::clean;
 use base ( 'WebService::Shippo::Creator',
            'WebService::Shippo::Fetcher',
            'WebService::Shippo::Lister',
+           'WebService::Shippo::CurrencyAware',
            'WebService::Shippo::Resource',
 );
 
 sub api_resource {'shipments'}
 
-sub rates 
+sub rates
 {
-    ;
+    my ( $invocant, $id, $currency, $params ) = @_;
+    if ( $currency ) {
+        my $validated = $invocant->validate_currency( $currency );
+        croak "Invalid currency code ($currency)"
+            unless $validated;
+        $currency = $validated;
+    }
+    else {
+        $currency ||= 'USD';
+    }
+    my $url = $invocant->url( "$id/rates/$currency" );
+    my $response = Shippo::Request->get( $url, $params );
+    return $invocant->construct_from( $response );
 }
 
 BEGIN {
