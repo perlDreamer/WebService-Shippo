1092ffc9b87ebf81dfa4894df457000d47c69281
Iain Campbell <cpanic@cpan.org>
2015-10-19 19:46:38 +0100

    Two new CarrierAccount mutators ("active" and "production") -
    They get/set the objects active/test states.

--- lib/WebService/Shippo/CarrierAccount.pm
+++ lib/WebService/Shippo/CarrierAccount.pm
@@ -3,6 +3,7 @@ use warnings;
 use MRO::Compat 'c3';
 
 package WebService::Shippo::CarrierAccount;
+use boolean ( 'boolean' );
 use base ( 'WebService::Shippo::Creator',
            'WebService::Shippo::Fetcher',
            'WebService::Shippo::Lister',
@@ -14,7 +15,25 @@ sub api_resource {'carrier_accounts'}    # why not "carrier/accounts", which
                                          # would be consistent with Customs
                                          # Declaration resource?
 
-package                                  # Hide from PAUSE
+sub active
+{
+    my ( $self, $bool ) = @_;
+    return boolean( $self->{active} ) unless @_ > 1;
+    my $update
+        = __PACKAGE__->update( $self->{object_id}, active => boolean( $bool ) );
+    return $self->refresh_from( $update );
+}
+
+sub production
+{
+    my ( $self, $bool ) = @_;
+    return boolean( !$self->{test} ) unless @_ > 1;
+    my $update
+        = __PACKAGE__->update( $self->{object_id}, test => boolean( !$bool ) );
+    return $self->refresh_from( $update );
+}
+
+package    # Hide from PAUSE
     WebService::Shippo::CarrierAccountList;
 use base ( 'WebService::Shippo::ObjectList' );
 
@@ -22,9 +41,8 @@ BEGIN {
     no warnings 'once';
     # Forcing the dev to always use CPAN's perferred "WebService::Shippo"
     # namespace is just cruel; allow the use of "Shippo", too.
-    *Shippo::CarrierAccount:: = *WebService::Shippo::CarrierAccount::;
-    *Shippo::CarrierAccountList::
-        = *WebService::Shippo::CarrierAccountList::;
+    *Shippo::CarrierAccount::     = *WebService::Shippo::CarrierAccount::;
+    *Shippo::CarrierAccountList:: = *WebService::Shippo::CarrierAccountList::;
 }
 
 1;
