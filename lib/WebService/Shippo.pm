9bdfc2a96fb48a18a9e815095b1c8186ca308424
Iain Campbell <cpanic@cpan.org>
2015-10-20 23:50:47 +0100

    Typo is synopsis

--- lib/WebService/Shippo.pm
+++ lib/WebService/Shippo.pm
@@ -139,11 +139,9 @@ see, documentation is rather thin on the ground; that, too, is coming.
     use strict;
     use WebService::Shippo;
     
-    # Following statement is not necessary if SHIPPO_TOKEN is set in
-    # your process environment.
     Shippo->api_key(PRIVATE-AUTH-TOKEN);
     
-    my $address1 = Shippo::Address->create({
+    my $address = Shippo::Address->create({
         object_purpose => 'PURCHASE',
         name           => 'John Smith',
         street1        => '6512 Greene Rd.',
