30451fc63b086e0fe75ebee03bfea967a3404295
Iain Campbell <cpanic@cpan.org>
2015-10-24 15:51:42 +0100

    Added "request_shipment_rates" and "get_shipment_rates"...
    The "request_shipment_rates" won't block but may not return a rate list,
    while "get_shipment_rates" will request the rates, block until the
    shipment object is rates prepared, then return those rates.

--- lib/WebService/Shippo/Shipment.pm
+++ lib/WebService/Shippo/Shipment.pm
@@ -6,74 +6,59 @@ package WebService::Shippo::Shipment;
 require WebService::Shippo::Rate;
 use Carp              ( 'croak' );
 use Params::Callbacks ( 'callbacks', 'callback' );
-use Time::HiRes       ( 'gettimeofday', 'tv_interval', 'usleep' );
-use Try::Tiny;
-use base ( 'WebService::Shippo::Creator',
-           'WebService::Shippo::Fetcher',
-           'WebService::Shippo::Lister',
-           'WebService::Shippo::CurrencyAware',
-           'WebService::Shippo::Resource',
+use Scalar::Util      ( 'blessed' );
+use base (
+    'WebService::Shippo::Resource',
+    'WebService::Shippo::Creator',
+    'WebService::Shippo::Fetcher',
+    'WebService::Shippo::Lister',
+    'WebService::Shippo::Currency',
+    'WebService::Shippo::Async',
 );
 
-sub api_resource {'shipments'}
+sub api_resource { 'shipments' }
 
-sub get_shipment_rates
+sub request_get_shipping_rates
 {
-    my ( $callbacks, $invocant, $id, $currency, @params ) = &callbacks;
-    croak "Expected an object_id"
-        unless $id;
-    if ( $currency ) {
-        my $validated = $invocant->validate_currency( $currency );
-        croak "Invalid currency code ($currency)"
-            unless $validated;
-        $currency = $validated;
+    my ( $callbacks, $invocant, $shipment_id, @params ) = &callbacks;
+    croak "Expected a shipment id"
+        unless $shipment_id;
+    my $currency;
+    if ( @params && @params % 2 ) {
+        ( $currency, @params ) = @params;
+        $currency = $invocant->validate_currency( $currency );
     }
-    else {
-        $currency ||= 'USD';
-    }
-    my $url = $invocant->url( "$id/rates/$currency" );
-    my $rates_object;
-    my $time = [ gettimeofday() ];
-    my $exception;
-    while ( !$rates_object ) {
-        try {
-            my $response = Shippo::Request->get( $url, @params );
-            $rates_object = $invocant->construct_from( $response, $callbacks );
-        }
-        catch {
-            $exception = $_;
-            usleep( 500 );
-        };
-        if ( $rates_object ) {
-            undef $exception;
-            last;
-        }
-        last if tv_interval( $time ) > WebService::Shippo::Rate->TIMEOUT;
-    }
-    croak $exception
-        if $exception;
-    return $rates_object;
-}
-
-sub rates
-{
-    my ( $callbacks, $invocant, @params ) = &callbacks;
-    croak "Shipment has no rates_url"
-        unless exists( $invocant->{rates_url} ) && $invocant->{rates_url};
-    my $response = Shippo::Request->get( $invocant->{rates_url} );
-    # Ok, prepend a callback to the callback queue ($callbacks), which will
-    # rebless our WebService::Shippo::Shipment objects (yes, that's what
-    # we got) as WebService::Shippo::Rate objects. This is much closer
-    # to the truth. The newly introduced callback stage will safely return
-    # an empty list or undef if either of those was passed.
+    my $rates_url = "$shipment_id/rates";
+    $rates_url .= "/$currency"
+        if $currency;
+    $rates_url = $invocant->url( $rates_url );
+    my $response = WebService::Shippo::Request->get( $rates_url, @params );
     unshift @$callbacks, callback {
         return unless @_;
         return $_[0] unless defined $_[0];
         return bless( $_[0], 'WebService::Shippo::Rate' );
     };
-    # And let's be sure to return whatever a collection of Rates is.
-    my $list = $invocant->construct_from( $response, $callbacks );
-    return bless( $list, WebService::Shippo::Rate->list_class );
+    my $rates = $invocant->construct_from( $response, $callbacks );
+    bless $rates, WebService::Shippo::Rate->list_class;
+    return $rates;
+}
+
+sub get_shipping_rates
+{
+    my ( $callbacks, $invocant, $shipment_id, @params ) = &callbacks;
+    my $shipment;
+    if (   blessed( $invocant )
+        && $invocant->id
+        && $invocant->id eq $shipment_id )
+    {
+        $shipment = $invocant;
+    }
+    else {
+        $shipment = WebService::Shippo::Shipment->fetch( $shipment_id );
+    }
+    &request_get_shipping_rates;
+    $shipment->wait_while_status_in( 'QUEUED', 'WAITING' );
+    return &request_get_shipping_rates;
 }
 
 package    # Hide from PAUSE
@@ -82,10 +67,12 @@ use base ( 'WebService::Shippo::ObjectList' );
 
 BEGIN {
     no warnings 'once';
+
     # Forcing the dev to always use CPAN's perferred "WebService::Shippo"
     # namespace is just cruel; allow the use of "Shippo", too.
     *Shippo::Shipment::     = *WebService::Shippo::Shipment::;
     *Shippo::ShipmentList:: = *WebService::Shippo::ShipmentList::;
+    *rates                  = *get_shipping_rates;
 }
 
 1;
