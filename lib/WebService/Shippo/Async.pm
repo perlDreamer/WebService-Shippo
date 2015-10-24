9921dc572b99b3f242b2ae69653c31978da4a17e
Iain Campbell <cpanic@cpan.org>
2015-10-24 15:05:00 +0100

    New parent class for entities affected by asynchronous workflows

---
+++ lib/WebService/Shippo/Async.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+use MRO::Compat 'c3';
+
+package WebService::Shippo::Async;
+use Carp        ( 'confess' );
+use List::Util  ( 'any' );
+use Time::HiRes ( 'gettimeofday', 'tv_interval', 'usleep' );
+
+{
+    my $value = 20;
+
+    sub timeout
+    {
+        my ( $invocant, $new_value ) = @_;
+        return $value unless @_ > 1;
+        $value = $new_value;
+        return $invocant;
+    }
+
+    sub timeout_exceeded
+    {
+        my ( $invocant, $tv_start ) = @_;
+        return tv_interval( $tv_start ) > $value;
+    }
+}
+
+sub wait_while_status_in
+{
+    my ( $invocant, @states ) = @_;
+    my $start_time = [ gettimeofday() ];
+    while ( !$invocant->timeout_exceeded( $start_time ) ) {
+        return $invocant
+            unless any {/^$invocant->{object_status}$/} @states;
+        usleep( 500 );
+        $invocant->refresh;
+    }
+    confess 'Asynchronus operation timed-out';
+}
+
+1;
