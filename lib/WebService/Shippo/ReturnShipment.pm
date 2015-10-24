e45237e1a26e8e84937dec6f16a0aa021558dac8
Iain Campbell <cpanic@cpan.org>
2015-10-24 15:45:00 +0100

    Reorder parent classes

--- lib/WebService/Shippo/ReturnShipment.pm
+++ lib/WebService/Shippo/ReturnShipment.pm
@@ -5,9 +5,9 @@ use MRO::Compat 'c3';
 package WebService::Shippo::ReturnShipment;
 use base ( 'WebService::Shippo::Shipment' );
 
-sub api_resource {'shipments'}
+sub api_resource { 'shipments' }
 
-package    # Hide from PAUSE
+package        # Hide from PAUSE
     WebService::Shippo::ReturnShipmentList;
 use base ( 'WebService::Shippo::ObjectList' );
 
@@ -15,9 +15,8 @@ BEGIN {
     no warnings 'once';
     # Forcing the dev to always use CPAN's perferred "WebService::Shippo"
     # namespace is just cruel; allow the use of "Shippo", too.
-    *Shippo::ReturnShipment:: = *WebService::Shippo::ReturnShipment::;
-    *Shippo::ReturnShipmentList::
-        = *WebService::Shippo::ReturnShipmentList::;
+    *Shippo::ReturnShipment::     = *WebService::Shippo::ReturnShipment::;
+    *Shippo::ReturnShipmentList:: = *WebService::Shippo::ReturnShipmentList::;
 }
 
 1;
