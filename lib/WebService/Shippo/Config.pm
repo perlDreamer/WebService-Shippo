ea7f35fdb704553747922764d52379565ba1a0de
Iain Campbell <cpanic@cpan.org>
2015-10-24 20:53:17 +0100

    Re-factor Config so that config and load_config both set things up

--- lib/WebService/Shippo/Config.pm
+++ lib/WebService/Shippo/Config.pm
@@ -41,52 +41,56 @@ our @SEARCH_PATH = (
 }
 
 {
-    my $value = __PACKAGE__->load_config_file;
+    my $config = undef;
 
     sub reload_config
     {
         my ( $invocant ) = @_;
-        $value = $invocant->load_config_file;
+        $invocant->load_config_file;
         return $invocant;
     }
 
     sub config
     {
         my ( $invocant, $new_value ) = @_;
-        return $value unless @_ > 1;
-        $value = $new_value;
+        return $config unless @_ > 1;
+        my $class = ref( $invocant ) || $invocant;
+        $config = $new_value;
+        my $default_token = $config->{default_token} || 'private_token';
+        my $api_key = $config->{$default_token};
+        Shippo::Resource->api_private_token( $config->{private_token} );
+        Shippo::Resource->api_public_token( $config->{public_token} );
+        Shippo::Resource->api_key( $api_key )
+            if $api_key;
+        bless $config, $class;
+        return $invocant;
+    }
+
+    sub load_config_file
+    {
+        my ( $invocant ) = @_;
+        my $class = ref( $invocant ) || $invocant;
+        my $config_file = $class->config_file;
+        # Return empty config if no config file exists
+        return bless( {}, $class )
+            unless $config_file && -e $config_file;
+        # Fetch the config content. By default, this should be defined in a
+        # file called Config.yml, located in the same folder as the Config.pm
+        # module.
+        open my $fh, '<:encoding(UTF-8)', $config_file
+            or croak "Can't open file '$config_file': $!";
+        my $config_yaml = do { local $/ = <$fh> };
+        close $fh;
+        # Return empty config if no YAML content was found
+        return bless( {}, $class )
+            unless $config_yaml;
+        # Parse the YAML content; use an empty config if that yields nothing.
+        $class->config( YAML::XS::Load( $config_yaml ) || {} );
         return $invocant;
     }
 }
 
-sub load_config_file
-{
-    my ( $class ) = @_;
-    $class = ref( $class ) || $class;
-    my $config_file = $class->config_file;
-    # Return empty config if no config file exists
-    return bless( {}, $class )
-        unless $config_file && -e $config_file;
-    # Fetch the config content. By default, this should be defined in a
-    # file called Config.yml, located in the same folder as the Config.pm
-    # module.
-    open my $fh, '<:encoding(UTF-8)', $config_file
-        or croak "Can't open file '$config_file': $!";
-    my $config_yaml = do { local $/ = <$fh> };
-    close $fh;
-    # Return empty config if no YAML content was found
-    return bless( {}, $class )
-        unless $config_yaml;
-    # Parse the YAML content; use an empty config if that yields nothing.
-    my $config        = YAML::XS::Load( $config_yaml ) || {};
-    my $default_token = $config->{default_token}       || 'private_token';
-    my $api_key       = $config->{$default_token};
-    Shippo::Resource->api_private_token( $config->{private_token} );
-    Shippo::Resource->api_public_token( $config->{public_token} );
-    Shippo::Resource->api_key( $api_key )
-        if $api_key;
-    return bless( $config, $class );
-}
+__PACKAGE__->load_config_file;
 
 BEGIN {
     no warnings 'once';
--- t/Shippo/040-ExtendedTests.t
+++ t/Shippo/040-ExtendedTests.t
@@ -161,7 +161,15 @@ my @tests = (
         is( $config_file_after, '/etc/foo', __TEST__ );
         WebService::Shippo::Config->config_file( $config_file_before );
         my $config_before = WebService::Shippo::Config->config;
-        WebService::Shippo::Config->config( { foo => 'bar', bar => 'baz' } );
+        WebService::Shippo::Config->config(
+            {   foo           => 'bar',
+                bar           => 'baz',
+                private_token => 'bar',
+                public_token  => 'foo'
+            }
+        );
+        is(WebService::Shippo::Resource->api_private_token, 'bar', __TEST__);
+        is(WebService::Shippo::Resource->api_public_token, 'foo', __TEST__);
         my $config_after = WebService::Shippo::Config->config;
         not_deeply( $config_before, $config_after, __TEST__ );
         WebService::Shippo::Config->reload_config;
