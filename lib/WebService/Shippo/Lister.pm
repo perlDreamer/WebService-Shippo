393c28b4735e96488e22ffd72c9a1e55454a1220
Iain Campbell <cpanic@cpan.org>
2015-11-03 13:26:55 +0000

    Consistent with other methods list and iterator take params as...
    a list l=of key-value pairs or a hash ref

--- lib/WebService/Shippo/Lister.pm
+++ lib/WebService/Shippo/Lister.pm
@@ -6,31 +6,32 @@ package WebService::Shippo::Lister;
 require WebService::Shippo::Request;
 use Params::Callbacks ( 'callbacks' );
 
-sub first_page
+sub list
 {
     my ( $callbacks, $invocant, @params ) = &callbacks;
-    my $response = WebService::Shippo::Request->get( $invocant->url, @params );
-    return $invocant->construct_from( $response, $callbacks );
-}
-
-sub all
-{
-    my ( $callbacks, $invocant, %params ) = &callbacks;
+    @params = ( {} )
+        unless @params;
+    my $params = ref( $params[0] ) ? $params[0] : {@params};
     my $response;
-    unless ( $params{results} ) {
+    unless ( $params->{results} ) {
         $response = WebService::Shippo::Request->get( $invocant->url, results => 1 );
-        $params{results} = $invocant->construct_from( $response )->count;
-        $params{results} = 3
-            unless $params{results};
+        $params->{results} = $invocant->construct_from( $response )->count;
+        $params->{results} = 3
+            unless $params->{results};
     }
-    $response = WebService::Shippo::Request->get( $invocant->url, %params );
+    $response = WebService::Shippo::Request->get( $invocant->url, $params );
     return $invocant->construct_from( $response, $callbacks );
 }
 
 sub iterator
 {
     my ( $callbacks, $invocant, @params ) = &callbacks;
-    my $list     = $invocant->first_page( @params );
+    @params = ( {} )
+        unless @params;
+    my $params = ref( $params[0] ) ? $params[0] : {@params};
+    $params->{results} = 5
+        unless $params->{results};
+    my $list     = $invocant->list( $params );
     my $index    = 0;
     my $iterator = sub {
         if ( $index == @{ $list->{results} } ) {
@@ -51,7 +52,7 @@ sub list_class
 
 BEGIN {
     no warnings 'once';
-    *list = *all;
+    *all = *list;
 }
 
 1;
