978bdcd1f65a5d12a4823a0134ee3bad44547b97
Iain Campbell <cpanic@cpan.org>
2015-11-03 20:05:39 +0000

    POD stubs added

--- lib/WebService/Shippo/Transaction.pm
+++ lib/WebService/Shippo/Transaction.pm
@@ -47,3 +47,60 @@ BEGIN {
 }
 
 1;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+WebService::Shippo::Transaction - Work with Shippo Transaction objects.
+
+=head1 DESCRIPTION
+
+A Transaction is the purchase of a shipment label for a given shipment
+rate. Transactions can be as simple as posting a rate identifier, but
+also allow you to define further label parameters, such as pickup and
+notifications.
+
+Transactions can only be created for rates that are less than 7 days
+old and whose C<object_purpose> attribute is B<PURCHASE>.
+
+Transactions are created asynchronously. The response time depends
+exclusively on the carrier's server.
+
+=head1 API DOCUMENTATION
+
+For more information about Transaction objects, consult the Shippo API
+documentation:
+
+=over 2
+
+=item * L<https://goshippo.com/docs/#transactions>
+
+=back
+
+=head1 REPOSITORY
+
+=over 2
+
+=item * L<https://github.com/cpanic/WebService-Shippo>
+
+=item * L<https://github.com/cpanic/WebService-Shippo/wiki>
+
+=back
+
+=head1 AUTHOR
+
+Iain Campbell <cpanic@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Iain Campbell.
+
+You may distribute this software under the terms of either the GNU General
+Public License or the Artistic License, as specified in the Perl README
+file.
+
+
+=cut
